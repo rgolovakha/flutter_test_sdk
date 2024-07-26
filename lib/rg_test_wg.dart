c743a036296612319770f0d1d15105490e66beca
Roman Golovakha <rgolovakha@gmail.com>
2024-07-26 09:42:51 +0300

    test

---
+++ lib/rg_test_wg.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class RgTestWg extends StatelessWidget {
+  const RgTestWg({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Text("Hello World 123");
+}
